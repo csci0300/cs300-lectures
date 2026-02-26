1764f8e3732437981bf6c3cdccf3d826fde8a9fb
Deepti Raghavan <deeptir@brown.edu>
2026-02-26 12:23:16 -0500

    Add Lecture 10 code

--- datarep/aligndemo.c
+++ datarep/aligndemo.c
@@ -3,11 +3,18 @@
 #include "hexdump.h"
 
 void f() {
-    void* data = malloc(19);
+    void* data = malloc(19 + 4);
     memset(data, 0xaa, 19);
+    int num = 0x12345678;
+    //int* p = (int*)(data + 19);
+    
 
-    printf("Address occupies addresses[%p, %p]\n", data, data + 19 - 1);
-    hexdump(data, 19);
+    //*p = 0x12345678;
+    char* p = (char*)(data + 19);
+    memcpy(p, (void *)(&num), 4);
+
+    printf("Address occupies addresses[%p, %p]\n", data, data + 19 + 4 - 1);
+    hexdump(data, 19+4);
 }
 
 int main() {
